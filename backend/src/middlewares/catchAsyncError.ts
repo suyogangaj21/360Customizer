9f0e3e6f279c412341ef514ea6e5d8684a714ef9
Suya6021 <smangaj@jspmrscoe.edu.in>
2024-10-17 23:54:21 +0530

    pdf uploaded

--- backend/src/middlewares/catchAsyncError.ts
+++
@@ -1,7 +0,0 @@
-import { Request, Response, NextFunction } from "express";
-let catchAsyncError =
-  (thefunc: Function) => (req: Request, res: Response, next: NextFunction) => {
-    Promise.resolve(thefunc(req, res, next)).catch(next);
-  };
-
-export default catchAsyncError;
